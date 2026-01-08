40ad400390e90751d917e550d8430a99801f3f6b
Abinandan V <abinandan.v@chargebee.com>
2026-01-08 02:19:36 -0500

    REV-15572 removed build folder

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { RetailCalendarFactory } from './retail_calendar';
-import { weekOfGregorianDate } from './gregorian_helpers';
-export { RetailCalendarFactory };
-export { weekOfGregorianDate };
-export * from './types';
--- dist/last_day_before_eom_except_leap_year.d.ts
+++
@@ -1,5 +0,0 @@
-import moment from 'moment';
-import { LastDayStrategy } from './types';
-export declare class LastDayBeforeEOMExceptLeapYearStrategy implements LastDayStrategy {
-    getLastDayForGregorianLastDay(lastDayOfGregorianYear: moment.Moment, lastDayOfIsoWeek: number): moment.Moment;
-}
